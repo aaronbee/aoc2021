5ee321886632f5395f94a868a37840f295aa8e62
Aaron Beitch <aaronb@arista.com>
2021-12-02 20:01:56 -0800

    day1a

---
+++ src/bin/day1a.rs
@@ -0,0 +1,26 @@
+use std::env;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let filename = &args[1];
+    let f = File::open(filename).unwrap();
+    let reader = BufReader::new(f);
+    let numbers: Vec<i32> = reader
+        .lines()
+        .filter_map(|l| l.ok())
+        .map(|l| l.parse::<i32>().unwrap())
+        .collect();
+
+    let iter1 = numbers.iter();
+    let mut iter2 = numbers.iter();
+    iter2.next();
+    let x: i32 = iter1
+        .zip(iter2)
+        .map(|(x, y)| if y > x { 1 } else { 0 })
+        .sum();
+    println!("{}", x);
+}
