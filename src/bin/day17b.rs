924aa3a22b352cc575f911824440f75ba33a7ce3
Aaron Beitch <aaronb@arista.com>
2022-01-09 20:31:12 -0800

    day17b: simplify

--- src/bin/day17b.rs
+++ src/bin/day17b.rs
@@ -1,32 +1,3 @@
-fn check_y(dy: i64, target_min: i64, target_max: i64) -> bool {
-    let mut y: i64 = 0;
-    let mut dy = if dy > 0 { -(dy + 1) } else { dy };
-    loop {
-        if y < target_min {
-            return false;
-        } else if y >= target_max {
-            return true;
-        }
-        y += dy;
-        dy -= 1;
-    }
-}
-
-fn check_x(mut dx: i64, target_min: i64, target_max: i64) -> bool {
-    let mut x = 0;
-    loop {
-        if x > target_max {
-            return false;
-        } else if x >= target_min {
-            return true;
-        } else if dx == 0 {
-            return false;
-        }
-        x += dx;
-        dx -= 1;
-    }
-}
-
 fn check_x_y(
     mut dx: i64,
     mut dy: i64,
@@ -42,6 +13,8 @@ fn check_x_y(
             return false;
         } else if x >= target_x_min && y <= target_y_max {
             return true;
+        } else if dx == 0 && x < target_x_min {
+            return false;
         }
         x += dx;
         y += dy;
@@ -53,21 +26,18 @@ fn check_x_y(
 }
 
 fn main() {
+    const TARGET_X_MIN: i64 = 102;
+    const TARGET_X_MAX: i64 = 157;
     const TARGET_Y_MIN: i64 = -146;
     const TARGET_Y_MAX: i64 = -90;
     const MAX_Y: i64 = 146;
-    let ys: Vec<i64> = (-MAX_Y..=MAX_Y)
-        .filter(|y| check_y(*y, TARGET_Y_MIN, TARGET_Y_MAX))
-        .collect();
 
-    const TARGET_X_MIN: i64 = 102;
-    const TARGET_X_MAX: i64 = 157;
     let mut count = 0;
-    for dx in (1..=TARGET_X_MAX).filter(|dx| check_x(*dx, TARGET_X_MIN, TARGET_X_MAX)) {
-        for dy in ys.iter() {
+    for dx in 1..=TARGET_X_MAX {
+        for dy in -MAX_Y..=MAX_Y {
             if check_x_y(
                 dx,
-                *dy,
+                dy,
                 TARGET_X_MIN,
                 TARGET_X_MAX,
                 TARGET_Y_MIN,
